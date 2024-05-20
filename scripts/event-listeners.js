132142812c4c26ad339ee46a50c0650947bb4754
Roman Druzhinin <karamount22@gmail.com>
2024-05-20 21:13:29 +0300

    feature: тотальная переработка таймера
    *Удалена функция refreshTimer()
    *Создан класс Timer, массивы с синтаксисом правильного написания дат перенесены в него как статические свойства
    *В класс добавлены методы и геттеры для получения даты из защищённого свойства _currentTimeBeforeEnd
    *Добавлена примесь eventMixin с методами для обработки событий происходящих в таймере
    *Теперь отображение времени изменяется за счет кастомных событий, вызывающихся при изменении времени в событии 'timeChange' с помощью метода refreshTimerDisplay(spansArray)

--- scripts/event-listeners.js
+++ scripts/event-listeners.js
@@ -1,4 +1,56 @@
+'use strict'
+const spansForDate = document.querySelectorAll('.span-for-timer-date');
+let timerBeforeEnd = new Timer(new Date(endDate));
+
+Object.assign(Timer.prototype, eventMixin);
+
+timerBeforeEnd.on('timeChange', () => timerBeforeEnd.refreshTimerDisplay(spansForDate));
+
+/*
+  Использование таймеров может показаться безумием, но они заставляют кастомные события тригериться правильно
+*/
+timerBeforeEnd.on('minutesChange', () => {
+  setTimeout( () => {
+    timerBeforeEnd.showMinutesBeforeEnd(spansForDate[4]);
+  }, 1000);
+});
+
+timerBeforeEnd.on('hoursChange', () => {
+  setTimeout( () => {
+    timerBeforeEnd.showHoursBeforeEnd(spansForDate[3]);
+  }, 1000);
+});
+
+timerBeforeEnd.on('daysChange', () => {
+  setTimeout( () => {
+    timerBeforeEnd.showDaysBeforeEnd(spansForDate[2]);
+  }, 1000);
+});
+
+timerBeforeEnd.on('monthsChange', () => {
+  setTimeout( () => {
+    timerBeforeEnd.showMonthsBeforeEnd(spansForDate[1]);
+  }, 1000);
+});
+
+timerBeforeEnd.on('yearsChange', () => {
+  setTimeout( () => {
+    timerBeforeEnd.showYearsBeforeEnd(spansForDate[0]);
+  }, 1000);
+});
+
 window.addEventListener('load', () => {
-  setInterval(refreshTimer, 1000, endDate);
+  timerBeforeEnd.showYearsBeforeEnd(spansForDate[0]);
+  timerBeforeEnd.showMonthsBeforeEnd(spansForDate[1]);
+  timerBeforeEnd.showDaysBeforeEnd(spansForDate[2]);
+  timerBeforeEnd.showHoursBeforeEnd(spansForDate[3]);
+  timerBeforeEnd.showMinutesBeforeEnd(spansForDate[4]);
+  timerBeforeEnd.showSecondsBeforeEnd(spansForDate[5]);
+
+  setInterval(() => {
+    timerBeforeEnd.changeTimeBeforeEnd(spansForDate);
+    timerBeforeEnd.showSecondsBeforeEnd(spansForDate[5]);
+  }, 1000);
+
   showDaysPassed();
 });
--- scripts/timer.js
+++ scripts/timer.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const dialogueContainerSpan = document.querySelector('.person-dialogue-container p span');
 const timerContainerParagraph = document.querySelector('.timer-container p');
 
@@ -22,30 +24,158 @@ function getPassedDays(startingDate) {
   return passedDays;
 }
 
-let years = ['лет', 'год', 'года', 'года', 'года', 'лет', 'лет', 'лет', 'лет', 'лет'];
-let months = ['месяцев', 'месяц', 'месяца', 'месяца', 'месяца', 'месяцев', 'месяцев', 'месяцев', 'месяцев', 'месяцев', 'месяцев', 'месяцев'];
-let days = ['дней', 'день', 'дня', 'дня', 'дня', 'дней', 'дней', 'дней', 'дней', 'дней'];
-let hours = ['часов', 'час', 'часа', 'часа', 'часа', 'часов', 'часов', 'часов', 'часов', 'часов'];
-let minutes = ['минут', 'минуту', 'минуты', 'минуты', 'минуты', 'минут', 'минут', 'минут', 'минут', 'минут'];
-let seconds = ['секунд', 'секунду', 'секунды', 'секунды', 'секунды', 'секунд', 'секунд', 'секунд', 'секунд', 'секунд'];
+class Timer {
+  static years = ['лет', 'год', 'года', 'года', 'года', 'лет', 'лет', 'лет', 'лет', 'лет'];
+  static months = ['месяцев', 'месяц', 'месяца', 'месяца', 'месяца', 'месяцев', 'месяцев', 'месяцев', 'месяцев', 'месяцев', 'месяцев', 'месяцев'];
+  static days = ['дней', 'день', 'дня', 'дня', 'дня', 'дней', 'дней', 'дней', 'дней', 'дней'];
+  static hours = ['часов', 'час', 'часа', 'часа', 'часа', 'часов', 'часов', 'часов', 'часов', 'часов'];
+  static minutes = ['минут', 'минуту', 'минуты', 'минуты', 'минуты', 'минут', 'минут', 'минут', 'минут', 'минут'];
+  static seconds = ['секунд', 'секунду', 'секунды', 'секунды', 'секунды', 'секунд', 'секунд', 'секунд', 'секунд', 'секунд'];
 
-function refreshTimer(endingDate) {
-  let timeBeforeEnd = new Date((endingDate - new Date() ) + Number( new Date('0000-01-01') ));
+  _currentTimeBeforeEnd = '';
 
-  let yearsBeforeEnd = String( timeBeforeEnd.getFullYear() ).at(-1);
-  let monthsBeforeEnd = timeBeforeEnd.getMonth();
-  let daysBeforeEnd = String( timeBeforeEnd.getDate() ).at(-1);
-  let hoursBeforeEnd = (10 <= timeBeforeEnd.getHours() && timeBeforeEnd.getHours() < 20) ? 9 : String( timeBeforeEnd.getHours() ).at(-1);
-  let minutesBeforeEnd = (10 <= timeBeforeEnd.getMinutes() && timeBeforeEnd.getMinutes() < 20) ? 9 : String( timeBeforeEnd.getMinutes() ).at(-1);
-  let secondsBeforeEnd = (10 <= timeBeforeEnd.getSeconds() && timeBeforeEnd.getSeconds() < 20) ? 9 : String( timeBeforeEnd.getSeconds() ).at(-1);
+  constructor(endingDate) {
+    this._currentTimeBeforeEnd = new Date((endingDate - new Date() ) + Number( new Date('0000-01-01') ));
+  }
 
+  changeTimeBeforeEnd() {
+    this._currentTimeBeforeEnd = new Date( this._currentTimeBeforeEnd.setSeconds( this._currentTimeBeforeEnd.getSeconds() - 1 ) );
+    this.trigger('timeChange', spansForDate);
+  }
 
-  timerContainerParagraph.innerHTML = `
-    Осталось ждать ${timeBeforeEnd.getFullYear()} ${years[yearsBeforeEnd]} 
-    ${timeBeforeEnd.getMonth()} ${months[monthsBeforeEnd]}
-    ${timeBeforeEnd.getDate()} ${days[daysBeforeEnd]} 
-    ${timeBeforeEnd.getHours()} ${hours[hoursBeforeEnd]} 
-    ${timeBeforeEnd.getMinutes()} ${minutes[minutesBeforeEnd]} 
-    ${timeBeforeEnd.getSeconds()} ${seconds[secondsBeforeEnd]}.
-  `
+  refreshTimerDisplay(spansArray) {
+    let currentSeconds = this._currentTimeBeforeEnd.getSeconds();
+    let currentMinutes = this._currentTimeBeforeEnd.getMinutes();
+    let currentHours = this._currentTimeBeforeEnd.getHours();
+    let currentDays = this._currentTimeBeforeEnd.getDate();
+    let currentMonths = this._currentTimeBeforeEnd.getMonth();
+
+    if(currentSeconds == 0) {
+      this.trigger('minutesChange', spansArray);
+    }
+
+    if(currentSeconds == 0 && currentMinutes == 0) {
+      this.trigger('hoursChange', spansArray);
+    }
+
+    if(
+      currentSeconds == 0 &&
+      currentMinutes == 0 &&
+      currentHours == 0
+    ) {
+      this.trigger('daysChange', spansArray);
+    }
+
+    if(
+      currentSeconds == 0 &&
+      currentMinutes == 0 &&
+      currentHours == 0 &&
+      currentDays == 0
+    ) {
+      this.trigger('monthsChange', spansArray);
+    }
+    
+    if(
+      currentSeconds == 0 &&
+      currentMinutes == 0 &&
+      currentHours == 0 &&
+      currentDays == 0 &&
+      currentMonths == 0
+    ) {
+      this.trigger('yearsChange', spansArray);
+    }
+  }
+
+  showYearsBeforeEnd(span) {
+    span.innerHTML = this.year;
+  }
+
+  showMonthsBeforeEnd(span) {
+    span.innerHTML = this.month;
+  }
+
+  showDaysBeforeEnd(span) {
+    span.innerHTML = this.day;
+  }
+
+  showHoursBeforeEnd(span) {
+    span.innerHTML = this.hour;
+  }
+
+  showMinutesBeforeEnd(span) {
+    span.innerHTML = this.minute;
+  }
+
+  showSecondsBeforeEnd(span) {
+    span.innerHTML = this.second;
+  }
+
+  get year() {
+    let yearsBeforeEnd = this._currentTimeBeforeEnd.getFullYear();
+
+    return yearsBeforeEnd + ' ' + Timer.years[ String(yearsBeforeEnd).at(-1) ];
+  }
+
+  get month() {
+    let monthsBeforeEnd = this._currentTimeBeforeEnd.getMonth();
+
+    return monthsBeforeEnd  + ' ' + Timer.months[monthsBeforeEnd ];
+  }
+
+  get day() {
+    let daysBeforeEnd = this._currentTimeBeforeEnd.getDate();
+
+    return daysBeforeEnd + ' ' + Timer.days[ (10 <= daysBeforeEnd && daysBeforeEnd < 20) ? 9 : String(daysBeforeEnd).at(-1) ];
+  }
+
+  get hour() {
+    let hoursBeforeEnd = this._currentTimeBeforeEnd.getHours();
+
+    return hoursBeforeEnd + ' ' + Timer.hours[ (10 <= hoursBeforeEnd && hoursBeforeEnd < 20) ? 9 : String(hoursBeforeEnd).at(-1) ];
+  }
+
+  get minute() {
+    let minutesBeforeEnd = this._currentTimeBeforeEnd.getMinutes();
+
+    return minutesBeforeEnd + ' ' + Timer.minutes[ (10 <= minutesBeforeEnd && minutesBeforeEnd < 20) ? 9 : String(minutesBeforeEnd).at(-1) ];
+  }
+
+  get second() {
+    let secondsBeforeEnd = this._currentTimeBeforeEnd.getSeconds();
+
+    return secondsBeforeEnd + ' ' + Timer.seconds[ (10 <= secondsBeforeEnd && secondsBeforeEnd < 20) ? 9 : String(secondsBeforeEnd).at(-1) ];
+  }
+}
+
+let eventMixin = {
+
+	on(eventName, handler) {
+		if(!this._eventHandlers) this._eventHandlers = {};
+		if(!this._eventHandlers[eventName]) {
+			this._eventHandlers[eventName] = [];
+		}
+		
+		this._eventHandlers[eventName].push(handler);
+	},
+	
+	off(eventName, handler) {
+		let handlers = this._eventHandlers && this._eventHandlers[eventName];
+		
+		if(!handlers) return;
+		
+		for(let i = 0; i < handlers.length; i++) {
+			if(handlers[i] === handler) {
+				handlers.splice(i--, 1);
+			}
+		}
+	},
+
+  
+  trigger(eventName, ...args) {
+	  if(!this._eventHandlers || !this._eventHandlers[eventName]) {
+		  return; 
+		}
+		
+		this._eventHandlers[eventName].forEach(handler => handler.apply(this, args));
+	}
 }
